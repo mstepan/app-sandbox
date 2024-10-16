a84c4e9572ce620b63c22aec473f138a460d74a3
mstepan <maksym.stepanenko@oracle.com>
2024-10-16 12:47:30 +0300

    Minor chnages

--- app_sandbox/app.py
+++ app_sandbox/app.py
@@ -1,74 +1,110 @@
-import collections
+import asyncio
 import math
 import sys
+import time
+from functools import reduce
+from pickle import FALSE
 
-Card = collections.namedtuple("Card", ["rank", "suite"])
+import aiohttp
+import ssl
 
-
-class Deck:
-    ranks = [str(single_rank) for single_rank in range(2, 11)] + ["J", "Q", "K", "A"]
-    suites = ["spades", "diamonds", "clubs", "hearts"]
-
-    def __init__(self):
-        self._card = [Card(rank, suite) for rank in self.ranks for suite in self.suites]
-
-    def __len__(self):
-        return len(self._card)
-
-    def __getitem__(self, idx):
-        return self._card[idx]
+semaphore = asyncio.Semaphore(2)
 
 
-#
-# suites order for sorting
-#
-suites_order = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+async def fetch_url(single_url):
+    async with semaphore:
+        async with aiohttp.ClientSession(
+            connector=aiohttp.TCPConnector(ssl=False)
+        ) as session:
+            async with session.get(single_url) as response:
+                print(f"{single_url}: status_code: {response.status}")
 
 
-# rank: 2,3,4,5,6,7,8,9,10,11, J,Q,K,A
-# suite: spades, hearts, diamonds, clubs
-def spades_highest(single_card: Card):
-    return (
-        Deck.ranks.index(single_card.rank) * len(suites_order)
-        + suites_order[single_card.suite]
-    )
+async def fetch_all_urls():
+    urls = [
+        "https://google.com",
+        "https://microsoft.com",
+        "https://oracle.com",
+        "https://reddit.com",
+        "https://dou.ua",
+        "https://stackoverflow.com",
+    ]
+
+    start_time = time.time()
+
+    await asyncio.gather(*(fetch_url(single_url) for single_url in urls))
+
+    end_time = time.time()
+
+    print(f"Elapsed time {end_time - start_time: .2f} seconds")
 
 
-class Vector:
-
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    def __eq__(self, other):
-        if not isinstance(other, self.__class__):
+async def is_prime(value: int) -> bool:
+    for factor in range(2, int(math.sqrt(value) + 1)):
+        if value % factor == 0:
             return False
-        return self.x == other.x and self.y == other.y
 
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-    def __repr__(self):
-        return f"Vector({self.x!r}, {self.y!r})"
-
-    def __abs__(self):
-        return math.hypot(self.x, self.y)
-
-    def __bool__(self):
-        return bool(self.x or self.y)
-
-    def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
-
-    def __mul__(self, scalar):
-        return Vector(self.x * scalar, self.y * scalar)
+    return True
 
 
-if __name__ == "__main__":
-    colors = ["red", "green", "blue"]
-    sizes = ["S", "M", "L"]
+def sieve_primes(value):
+    values = [True for _ in range(0, value + 1)]
+    values[0] = False
+    values[1] = False
 
-    for val in (f"{color} {size}" for color in colors for size in sizes):
-        print(val)
+    for i in range(2, len(values)):
+        if values[i]:
+            composite = i + i
 
-    print(f"Python: {sys.version}")
+            while composite < len(values):
+                values[composite] = False
+                composite += i
+
+    return [i for i, val in enumerate(values) if val is True]
+
+
+async def prime_factors(value: int) -> list[int]:
+
+    primes = sieve_primes(value + 1)
+
+    left_to_factor = value
+
+    factors = []
+
+    while left_to_factor != 1:
+        for single_prime in primes:
+            if left_to_factor % single_prime == 0:
+                factors.append(single_prime)
+                left_to_factor //= single_prime
+                break
+
+    return factors
+
+
+"""
+Main function
+"""
+
+
+async def main():
+
+    value = 123456
+
+    start_time = time.time()
+    results = await prime_factors(value)
+    end_time = time.time()
+
+    if value != reduce(lambda x, y: x * y, results):
+        print("Incorrect result calculated")
+    else:
+        print(f"Factors for '{value}' are {results}")
+
+    print(f"Elapsed time: {end_time - start_time:.2f} second")
+
+    print(f"Python: {sys.version}, {ssl.OPENSSL_VERSION}")
+
+
+"""
+Run event loop using 'main' coroutine
+"""
+asyncio.run(main())
